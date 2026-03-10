550ec94ffcbe0aa4f5b93d0d744eab4fd95b258c
JhonWick9191 <singhaditya5329@gmail.com>
2026-03-10 14:18:31 +0530

    adding code for inviation send to the user

--- Backend/Controllers/ApproveUser.js
+++ Backend/Controllers/ApproveUser.js
@@ -1,6 +1,6 @@
 // Approve or Deny User
 const userModel = require("../Models/UserModel");
-
+const mailSender = require("../utils/MailSender")
 async function approveOrDenyUser(req, res) {
     try {
         const userId = req.params.id;
@@ -25,7 +25,38 @@ async function approveOrDenyUser(req, res) {
                 success: true,
                 message: "User approved successfully!"
             });
+
+
+
         }
+
+
+        await mailSender(
+            user.email,
+            "Your ID has been verified. You can now login",
+            `
+  <div style="font-family: Arial, sans-serif; text-align:center;">
+    
+    <img 
+      src="https://pub-2b1f343884754239835449d2250d43a1.r2.dev/2.png"
+      alt="Music and More"
+      style="width:100%; max-width:600px; border-radius:8px;"
+    />
+
+    <h1>Welcome ${user.first_name} 🎵</h1>
+
+    <p>Your ID has been verified successfully.</p>
+
+    <p>You can now login and explore <b>Music and More</b>.</p>
+
+    <a href="https://yourwebsite.com/login"
+      style="display:inline-block; margin-top:20px; padding:12px 25px; background:#ff6b00; color:white; text-decoration:none; border-radius:5px;">
+      Login Now
+    </a>
+
+  </div>
+  `
+        );
 
         // DENY → DELETE USER
         if (action === "deny") {
@@ -36,6 +67,7 @@ async function approveOrDenyUser(req, res) {
                 message: "User denied and deleted successfully!"
             });
         }
+
 
         return res.status(400).json({
             success: false,
